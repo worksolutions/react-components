4c081b18e72af390fd0962a1e233d577b4e2f073
Dulatova Maria <m.dulatova@worksolutions.ru>
2021-02-16 14:43:30 +0300

    Доработка компонента "Лоадер"

--- src/primitives/Spinner/index.tsx
+++ src/primitives/Spinner/index.tsx
@@ -4,20 +4,54 @@ import { child, getColor } from "../../styles";
 
 import { Colors } from "../../constants/colors";
 
+enum Size {
+    'extra-small',
+    small,
+    medium,
+    large,
+    'extra-large',
+    'custom'
+}
+
+type SizeType = keyof typeof Size;
+
+type SizeWidth = {
+    [key in Exclude<SizeType, "custom">]: number;
+}
+
+const sizeWidth: SizeWidth = {
+    'extra-small': 10,
+    small: 14,
+    medium: 24,
+    large: 45,
+    'extra-large': 90
+};
+
 export interface SpinnerInterface {
   color?: Colors;
   className?: string;
+  size: SizeType;
+  width?: number;
 }
 
+const getSpinnerWidth = ({size, width}: SpinnerInterface) => {
+    if (size === 'custom') {
+        return width;
+    }
+    return sizeWidth[size]
+};
+
 const StyledSpinner = styled.div.attrs({ className: "loader" })<Required<SpinnerInterface>>`
+   width: ${(props) => getSpinnerWidth(props)}px;
+
   .path {
     stroke: ${(props) => getColor(props.color)};
   }
 `;
 
-const Spinner = function (props: SpinnerInterface) {
+const Spinner = function ({color, className, size = 'medium', width}: SpinnerInterface) {
   return (
-    <StyledSpinner {...(props as any)}>
+    <StyledSpinner {...({color, className, size, width} as any)}>
       <svg className="circular" viewBox="25 25 50 50">
         <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10" />
       </svg>
