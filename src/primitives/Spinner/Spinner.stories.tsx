b31447a4f0e6f0664538289bea46f9ad11aa59d3
edelgarat <edelgarat@yandex.ru>
2021-02-19 13:12:38 +0300

    Обновление контрола цветов

--- src/primitives/Spinner/Spinner.stories.tsx
+++ src/primitives/Spinner/Spinner.stories.tsx
@@ -1,31 +1,28 @@
 import React from "react";
-import {Story} from "@storybook/react/types-6-0";
+import { Story } from "@storybook/react/types-6-0";
 
-import {storybookWrapper} from "storybookWrapper";
-import {colorControl, selectControl} from "storyHelpers";
+import { storybookWrapper } from "storybookWrapper";
+import { colorControl, selectControl } from "storyHelpers";
 
-import Spinner, {SpinnerInterface, SpinnerSize} from "./index";
+import Spinner, { SpinnerInterface, SpinnerSize } from "./index";
 
 export default {
-  title: "Loader",
+  title: "Spinner",
   component: Spinner.type,
   decorators: [storybookWrapper],
   argTypes: {
     color: colorControl(),
-    size: selectControl(Object.keys(SpinnerSize))
-
+    size: selectControl(Object.keys(SpinnerSize)),
   },
 };
 
 const LoadTemplate: Story<SpinnerInterface> = (props) => {
-  return (
-    <Spinner {...props} />
-  )
+  return <Spinner {...props} />;
 };
 
 export const Loader = LoadTemplate.bind({});
 
 Loader.args = {
   color: "gray-blue/09",
-  size: SpinnerSize.medium
+  size: SpinnerSize.medium,
 };
