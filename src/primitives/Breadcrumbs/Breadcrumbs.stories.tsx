30a9c8e95e366960a34338ca737d7fe78fb90e93
Dulatova Maria <m.dulatova@worksolutions.ru>
2021-02-20 10:58:18 +0300

    Рендер BreadcrumbsItem как children, добавлена возможность выбора цвета бейджа

--- src/primitives/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/primitives/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -4,19 +4,20 @@ import { Story } from "@storybook/react/types-6-0";
 import { storybookWrapper } from "storybookWrapper";
 
 import Breadcrumbs, { BreadcrumbsProps } from "./index";
+import BreadcrumbsItem, { BreadcrumbsItemProps } from "./BreadcrumbsItem";
+import { colorControl } from "../../storyHelpers";
 
 export default {
   title: "Breadcrumbs",
   component: Breadcrumbs.type,
+  argTypes: {
+    badgeColor: colorControl(),
+  },
   decorators: [storybookWrapper],
 };
 
-const BreadcrumbsTemplate: Story<BreadcrumbsProps> = (props) => <Breadcrumbs {...props} />;
-
-export const Default = BreadcrumbsTemplate.bind({});
-
-Default.args = {
-  items: [
+const BreadcrumbsTemplate: Story<BreadcrumbsProps> = (props) => {
+  const items: Omit<BreadcrumbsItemProps, "isLastItem" | "withBadge" | "badgeColor">[] = [
     { to: "/", text: "Главная" },
     { to: "/catalog", text: "Каталог" },
     { to: "/catalog/category", text: "Страница категории" },
@@ -24,6 +25,31 @@ Default.args = {
     { to: "/catalog/category/product/item", text: "Страница конкретного товара" },
     { to: "/catalog/category/product/item/reviews", text: "Страница отзывов об этом конкретном товаре" },
     { to: "/catalog/category/product/item/reviews/561234", text: "Страница конкретного отзыва" },
-  ],
+  ];
+
+  return (
+    <Breadcrumbs {...props}>
+      {items.map((item, index) => {
+        const isLastItem = index === items.length - 1;
+        const { to, text } = item;
+        return (
+          <BreadcrumbsItem
+            key={index}
+            isLastItem={isLastItem}
+            text={text}
+            to={to}
+            withBadge={props.withBadge}
+            badgeColor={props.badgeColor}
+          />
+        );
+      })}
+    </Breadcrumbs>
+  );
+};
+
+export const Default = BreadcrumbsTemplate.bind({});
+
+Default.args = {
   withBadge: true,
+  badgeColor: "blue/05",
 };
--- src/primitives/Breadcrumbs/index.tsx
+++ src/primitives/Breadcrumbs/index.tsx
@@ -1,125 +1,17 @@
 import React from "react";
 
-import TypographyLink, { TypographyLinkProps } from "primitives/Typography/TypographyLink";
 import Wrapper from "../Wrapper";
-import Typography from "../Typography";
-import {
-  ai,
-  borderRadius,
-  color,
-  disableDecoration,
-  disableOutline,
-  lineHeight,
-  padding,
-  transition,
-  display,
-  child,
-  hover,
-  backgroundColor,
-  marginRight,
-  focus,
-  boxShadow,
-  fontWeight,
-  fontSize,
-  content,
-  position,
-  right,
-  flexWrap,
-  marginBottom,
-  top,
-  bottom,
-  left,
-  flex,
-  fullWidth,
-} from "styles";
-import { duration160 } from "../..";
-import Icon from "../Icon";
-
-interface BreadcrumbsItem extends Omit<TypographyLinkProps, "children"> {
-  text: string;
-}
+import { display, flexWrap, fullWidth } from "styles";
+import { Colors } from "constants/colors";
 
 export interface BreadcrumbsProps {
-  items: BreadcrumbsItem[];
-  withBadge?: boolean;
+  children: React.ReactNode;
+  withBadge: boolean;
+  badgeColor: Colors;
 }
 
-interface BreadcrumbsWrapperProps {
-  item: BreadcrumbsItem;
-  isLastElement: boolean;
-  children?: React.ReactNode;
-}
-
-function Breadcrumbs({ items, withBadge = false }: BreadcrumbsProps) {
-  const breadcrumbsItemCommonStyles = [
-    position("relative"),
-    display("block"),
-    ai("center"),
-    padding(withBadge ? "2px 8px 3px 24px" : "2px 8px 3px"),
-    borderRadius(4),
-    fontSize(14),
-    lineHeight("1.5"),
-    fontWeight(500),
-  ];
-
-  const BreadcrumbsWrapper = ({ item, isLastElement, children }: BreadcrumbsWrapperProps) => {
-    if (isLastElement) {
-      return <Typography styles={breadcrumbsItemCommonStyles}>{children}</Typography>;
-    }
-
-    return (
-      <TypographyLink
-        to={item.to}
-        styles={[
-          breadcrumbsItemCommonStyles,
-          hover(backgroundColor("gray-blue/01")),
-          focus(boxShadow([0, 0, 0, 2, "blue/05"])),
-          transition(`background-color, box-shadow ${duration160}`),
-          disableOutline,
-          disableDecoration,
-        ]}
-      >
-        {children}
-      </TypographyLink>
-    );
-  };
-
-  return (
-    <Wrapper styles={[display("flex"), flexWrap, fullWidth]}>
-      {items.map((item, index) => {
-        const isLastElement = index === items.length - 1;
-        return (
-          <Wrapper key={index} styles={[position("relative"), flex, lineHeight(1), marginRight(10), marginBottom(10)]}>
-            <BreadcrumbsWrapper item={item} isLastElement={isLastElement}>
-              {withBadge && (
-                <Wrapper styles={[marginRight(8)]}>
-                  <Icon
-                    width={8}
-                    height={8}
-                    icon="badge"
-                    color="blue/05"
-                    styles={[position("absolute"), top(8), left(8)]}
-                  />
-                </Wrapper>
-              )}
-
-              <Typography
-                styles={[
-                  position("relative"),
-                  display("inline"),
-                  color("gray-blue/05"),
-                  !isLastElement &&
-                    child([content("/"), position("absolute"), bottom(-1), right(-16), display("block")], "&:after"),
-                ]}
-              >
-                {item.text}
-              </Typography>
-            </BreadcrumbsWrapper>
-          </Wrapper>
-        );
-      })}
-    </Wrapper>
-  );
+function Breadcrumbs({ withBadge = false, badgeColor = "blue/05", children }: BreadcrumbsProps) {
+  return <Wrapper styles={[display("flex"), flexWrap, fullWidth]}>{children}</Wrapper>;
 }
 
 export default React.memo(Breadcrumbs);
