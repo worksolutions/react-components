adf806f1c7038851fb36b12368123961a092d852
Gavrilov Grigoriy <g.gavrilov@worksolutions.ru>
2021-03-12 10:23:38 +0300

    добавил возможность смены offset для tooltip

--- src/primitives/Tooltip/TooltipContainer.tsx
+++ src/primitives/Tooltip/TooltipContainer.tsx
@@ -1,20 +1,42 @@
-import React, { useCallback } from "react";
-import PopperManager from "../PopperManager";
+import React, { useCallback, useEffect, useState } from "react";
 import { Placement } from "@popperjs/core/lib/enums";
+
+import PopperManager from "../PopperManager";
 import Tooltip from "./index";
-interface TooltipContainerProps {
+
+export interface TooltipContainerProps {
   placement?: Placement;
   tooltipText: string;
-  tooltipStyles: any;
+  tooltipStyles?: any;
+  haveArrow: boolean;
   children: (toggleVisible: () => void, visible: boolean) => React.ReactNode;
 }
-function TooltipContainer({ children, placement, tooltipText, tooltipStyles }: TooltipContainerProps) {
+
+function TooltipContainer({ haveArrow, placement, tooltipText, tooltipStyles, children }: TooltipContainerProps) {
+  const [offset, setOffset] = useState<[number, number] | undefined>();
+  
   const popperElement = useCallback(() => <Tooltip styles={tooltipStyles}>{tooltipText}</Tooltip>, [
     tooltipStyles,
     tooltipText,
   ]);
 
-  return <PopperManager haveArrow placement={placement} referenceElement={children} popperElement={popperElement} />;
+  useEffect(() => {
+    if (haveArrow) {
+      setOffset(undefined);
+      return;
+    }
+    setOffset([0, 14]);
+  }, [haveArrow]);
+
+  return (
+    <PopperManager
+      haveArrow={haveArrow}
+      placement={placement}
+      referenceElement={children}
+      popperElement={popperElement}
+      offset={offset}
+    />
+  );
 }
 
 export default React.memo(TooltipContainer);
--- src/primitives/Tooltip/story/TooltipStory.stories.tsx
+++ src/primitives/Tooltip/story/TooltipStory.stories.tsx
@@ -2,27 +2,28 @@ import React from "react";
 import { Story } from "@storybook/react/types-6-0";
 import { placements } from "@popperjs/core/lib/enums";
 
-import Tooltip, { TooltipProps } from "../index";
-import TooltipContainer from "../TooltipContainer";
+import Tooltip from "../index";
+import TooltipContainer, { TooltipContainerProps } from "../TooltipContainer";
 import Wrapper from "../../Wrapper";
 import { BaseInput } from "../../Input/story/Input.stories";
+
 import { selectControl } from "../../../storybook/storyHelpers";
 import { left, marginRight, position, top, transform } from "../../../styles";
 
 export default {
   title: "Tooltip",
-  component: Tooltip.type,
+  component: TooltipContainer.type,
   argTypes: {
     placement: selectControl(placements),
   },
 };
 
-const Template: Story<TooltipProps> = (props) => {
+const Template: Story<TooltipContainerProps> = (props) => {
   return (
     <Wrapper
       styles={[position("absolute"), top("40%"), left("50%"), marginRight("-50%"), transform("translate(-50%, -50%)")]}
     >
-      <TooltipContainer {...props} tooltipText="tooltipText">
+      <TooltipContainer {...props}>
         {(toggleVisible) => (
           <Wrapper onClick={toggleVisible}>
             <BaseInput value="baseValue" onChange={() => {}} />
@@ -34,3 +35,8 @@ const Template: Story<TooltipProps> = (props) => {
 };
 
 export const Default = Template.bind({});
+
+Default.args = {
+  haveArrow: true,
+  tooltipText: "text",
+};
