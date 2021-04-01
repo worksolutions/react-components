05de5e0fa7be4bb2663ab6467ebbbab401962e2f
edelgarat <edelgarat@yandex.ru>
2021-04-01 11:48:31 +0300

    Обновление zIndex у лоадинга и тоста

--- src/constants/zIndexes.ts
+++ src/constants/zIndexes.ts
@@ -2,5 +2,5 @@ import { zIndex } from "../styles";
 
 export const zIndex_popup = zIndex(6);
 export const zIndex_modal = zIndex(8);
-export const zIndex_toast = zIndex(9);
-export const zIndex_loadingProvider = zIndex(10);
+export const zIndex_loadingProvider = zIndex(9);
+export const zIndex_toast = zIndex(10);
