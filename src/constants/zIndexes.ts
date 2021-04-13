c248adc156f85f1fd7a9ebbb762c947e2089dfb8
edelgarat <edelgarat@yandex.ru>
2021-04-13 21:47:20 +0300

    Обновление z-index

--- src/constants/zIndexes.ts
+++ src/constants/zIndexes.ts
@@ -1,6 +1,7 @@
 import { zIndex } from "../styles";
 
-export const zIndex_popup = zIndex(6);
-export const zIndex_modal = zIndex(8);
-export const zIndex_loadingProvider = zIndex(9);
-export const zIndex_toast = zIndex(10);
+export const zIndex_loadingProvider = zIndex(8);
+export const zIndex_upper_loadingProvider = zIndex(9);
+export const zIndex_popup = zIndex(10);
+export const zIndex_modal = zIndex(20);
+export const zIndex_toast = zIndex(30);
