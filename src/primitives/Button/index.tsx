40deddc10a48522ce84e5d12b9e245068f5a90ec
edelgarat <edelgarat@yandex.ru>
2021-04-23 09:44:32 +0300

    Добавление пропа onClickWhenDisabled в кнопку

--- src/primitives/Button/index.tsx
+++ src/primitives/Button/index.tsx
@@ -1,5 +1,5 @@
-import React, { ButtonHTMLAttributes, Ref } from "react";
-import { preventDefault } from "@worksolutions/react-utils";
+import React, { ButtonHTMLAttributes, Ref, SyntheticEvent } from "react";
+import { preventDefaultHandler } from "@worksolutions/react-utils";
 
 import Wrapper from "../Wrapper";
 import ButtonWrapper, { BaseButtonWrapperInterface } from "./ButtonWrapper";
@@ -14,6 +14,7 @@ export interface ButtonInterface extends BaseButtonWrapperInterface {
   preventDefault?: boolean;
   children?: React.ReactNode;
   onClick?: () => void;
+  onClickWhenDisabled?: () => void;
 }
 
 function Button(
@@ -21,38 +22,72 @@ function Button(
     as = "button",
     nativeType = "button",
     children,
-    onClick,
     preventDefault: preventDefaultProp = true,
     className,
     tabIndex,
+    onClick,
+    onClickWhenDisabled,
     ...buttonWrapperProps
   }: ButtonInterface,
   ref: Ref<HTMLElement>,
 ) {
-  return (
-    <ButtonWrapper {...buttonWrapperProps}>
-      {(styles, iconLeft, iconRight) => {
-        const loading = buttonWrapperProps.loadingLeft || buttonWrapperProps.loadingRight;
-        const clickHandler = loading ? undefined : onClick;
-        return (
-          <Wrapper
-            className={className}
-            ref={ref}
-            tabIndex={tabIndex}
-            type={nativeType}
-            as={as}
-            styles={styles}
-            disabled={buttonWrapperProps.disabled}
-            onClick={clickHandler && (preventDefaultProp ? preventDefault(clickHandler) : clickHandler)}
-          >
-            {iconLeft}
-            {loading ? buttonWrapperProps.loadingText || children : children}
-            {iconRight}
-          </Wrapper>
-        );
-      }}
-    </ButtonWrapper>
+  const clickHandler = React.useCallback(
+    (event: SyntheticEvent<HTMLButtonElement, MouseEvent>) => {
+      const loading = buttonWrapperProps.loadingLeft || buttonWrapperProps.loadingRight;
+      if (loading) return;
+      if (preventDefaultProp) preventDefaultHandler(event);
+
+      if (buttonWrapperProps.disabled) {
+        if (onClickWhenDisabled) onClickWhenDisabled();
+        return;
+      }
+
+      if (onClick) onClick();
+    },
+    [
+      buttonWrapperProps.disabled,
+      buttonWrapperProps.loadingLeft,
+      buttonWrapperProps.loadingRight,
+      onClick,
+      onClickWhenDisabled,
+      preventDefaultProp,
+    ],
   );
+
+  const buttonElement = React.useCallback(
+    (styles: any, iconLeft: React.ReactNode, iconRight: React.ReactNode) => {
+      const loading = buttonWrapperProps.loadingLeft || buttonWrapperProps.loadingRight;
+      return (
+        <Wrapper
+          ref={ref}
+          className={className}
+          tabIndex={tabIndex}
+          type={nativeType}
+          as={as}
+          styles={styles}
+          onClick={clickHandler}
+        >
+          {iconLeft}
+          {loading ? buttonWrapperProps.loadingText || children : children}
+          {iconRight}
+        </Wrapper>
+      );
+    },
+    [
+      as,
+      buttonWrapperProps.loadingLeft,
+      buttonWrapperProps.loadingRight,
+      buttonWrapperProps.loadingText,
+      children,
+      className,
+      clickHandler,
+      nativeType,
+      ref,
+      tabIndex,
+    ],
+  );
+
+  return <ButtonWrapper {...buttonWrapperProps}>{buttonElement}</ButtonWrapper>;
 }
 
 export default React.memo(React.forwardRef(Button));
